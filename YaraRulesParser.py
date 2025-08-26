9b64365e0d3a66c9a9b35705bf4deb9349c2dba2
zer0daysec <150989298+zer0daysec@users.noreply.github.com>
2025-08-26 13:59:38 +0800

    Update YaraRulesParser.py

--- YaraRulesParser.py
+++ YaraRulesParser.py
@@ -1,5 +1,8 @@
 import re
 
+from pathlib import Path
+from datetime import datetime
+
 def parse_yara_file(filepath):
     rules = []
 
@@ -52,20 +55,55 @@ def parse_yara_file(filepath):
 
     return rules
 
-if __name__ == "__main__":
-    filepath = './Win32/Ransom/Zuresq.yara'
-    parsed = parse_yara_file(filepath)
+def trans_rules(folder_path, target_path):
+    folder_path = Path(folder_path)
+    target_path = Path(target_path)
+    for file in folder_path.iterdir():
+        if file.is_file():    
+            parsed = parse_yara_file(file)
+            target_file = target_path / file.name
+            with open(target_file, 'w', encoding='utf-8') as f:
+                for rule in parsed:
+                    f.write(f"rule {rule['name']} : {file.name.split('.')[0]}\n")
+                    f.write("{\n")
+                    f.write(f"{' '*4}meta:\n")
+                    f.write(f"{' '*8}description = \"_{rule['name']}_\"\n")
+                    f.write(f"{' '*8}score = 70\n")
+                    f.write(f"{' '*8}author = \"xxxxxx\"\n")
+                    f.write(f"{' '*8}create_time = \"{datetime.now().strftime('%Y-%m-%d')}\"\n\n")
+                    f.write(f"{' '*4}strings:\n")
+                    for s in rule['strings']:
+                        f.write(f"{' '*8}{s['name']} = {s['value']}   {s['modifiers']}\n")
+                    f.write(f"\n{' '*4}condition: \n")
+                    # f.write(f"{' '*8}(uint16(0) == 0x5a4d) and {rule['condition']}\n")    # windows
+                    # f.write(f"{' '*8}(uint32(0) == 0x464c457f) and {rule['condition']}\n")  # linux
+                    f.write(f"{' '*8}(uint32(0) == 0xfeedface or uint32(0) == 0xfeedfacf or uint32(0) == 0xcafebabe or uint32(0) == 0xcafebabf) and {rule['condition']}\n")  # MacOS
+                    f.write("}\n\n")
 
-    for rule in parsed:
-        """
-        print(f"Rule: {rule['name']}")
-        print(f"  Tags: {rule['tags']}")
-        print(f"  Meta:")
-        for k, v in rule['meta'].items():
-            print(f"    {k}: {v}")
-        """
-        print(f"  Strings:")
-        for s in rule['strings']:
-            print(f"    {s['name']} = {s['value']}   {s['modifiers']}")
-        print(f"  Condition: {rule['condition']}")
-        print("=" * 60)
+# 提取索引
+# platform: Win32、Win64、Linux etc
+# category: Trojan、Worm、Ransom etc
+def extract_index(folder_path, target_file, platform, category):
+    folder_path = Path(folder_path)
+    for file in folder_path.iterdir():
+        if file.is_file():
+            target_file = category + '_index.yar'
+            with open(target_file, 'a', encoding='utf-8') as f:
+                f.write(f"include \"./{category}/{platform}/{file.name}\"\n")
+
+# 提取命名空间
+def extract_namespace(folder_path, target_file, category):
+    folder_path = Path(folder_path)
+    for file in folder_path.iterdir():
+        if file.is_file():    
+            parsed = parse_yara_file(file)
+            with open(target_file, 'a', encoding='utf-8') as f:
+                for rule in parsed:
+                    f.write(f"{' '*4}\"_{rule['name']}_\": \"{file.name.split('.')[0]}{category}\",\n")
+
+if __name__ == "__main__":
+    folder_path="/Users/zer0daysec/GitRepo/defender2yara/MacOS_X/Trojan"
+    target_path="./rules/Trojan/MacOS_X"
+    # trans_rules(folder_path, target_path)
+    # extract_index(folder_path, "./r", "MacOS_X", "Trojan")
+    extract_namespace(folder_path, "./namespace.json", " Trojan")
